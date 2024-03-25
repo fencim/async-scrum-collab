c65da4b84793685f2fa632284effcac76d19d5bd
fencim <fencim@gmail.com>
2024-03-26 00:02:26 +0800

    check vote if moderator

--- src/components/chat/ChatVoteMessage.vue
+++ src/components/chat/ChatVoteMessage.vue
@@ -2,41 +2,38 @@
   <base-chat-message :msg="msg" :curr-user="currUser" @reply-to="onReply">
     <div style="min-width: 150px" v-if="msg.type == 'vote'">
       <span v-html="msg.message"></span>
-      <q-badge v-if="revealVotes" class="bg-green">{{ msg.vote }}</q-badge>
+      <q-badge v-if="revealVotes || showVote" class="q-ml-xs bg-green">{{
+        msg.vote
+      }}</q-badge>
+      <q-toggle
+        dense
+        class="q-ml-xs"
+        v-else-if="activeStore.canUserModerate"
+        v-model="showVote"
+        icon="poll"
+      />
+      <q-icon name="show"></q-icon>
     </div>
   </base-chat-message>
 </template>
 
-<script lang="ts">
-import { defineComponent, PropType } from 'vue';
+<script lang="ts" setup>
+import { ref } from 'vue';
 import BaseChatMessage from './BaseChatMessage.vue';
 import { Convo } from 'src/entities';
-
-export default defineComponent({
-  name: 'VoteChatMessage',
-  components: { BaseChatMessage },
-  emits: ['replyTo'],
-  props: {
-    msg: {
-      type: Object as PropType<Convo>,
-      required: true,
-    },
-    replyTo: {
-      type: Object as PropType<Convo>,
-    },
-    currUser: {
-      type: String,
-    },
-    revealVotes: {
-      type: Number,
-    },
-  },
-  methods: {
-    onReply(m: Convo) {
-      this.$emit('replyTo', m);
-    },
-  }, //methods
-});
+import { useActiveStore } from 'src/stores/active.store';
+const activeStore = useActiveStore();
+const $emit = defineEmits(['replyTo']);
+defineProps<{
+  msg: Convo;
+  replyTo: Convo;
+  currUser: string;
+  revealVotes: number;
+}>();
+const showVote = ref(false);
+function onReply(m: Convo) {
+  $emit('replyTo', m);
+}
 </script>
 
 <style></style>
